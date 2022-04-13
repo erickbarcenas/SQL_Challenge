e13cd915dfc36a64fa710ab54efe1f864d2cda41
Erick Bárcenas <barcenas.dev@gmail.com>
2022-04-12 20:59:22 -0500

    Tables are updated, the schema and the script that registers a new payment are created

--- schema_db.sql
+++ schema_db.sql
@@ -6,13 +6,14 @@ CREATE TABLE IF NOT EXISTS food_store.product
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     product character varying(50),
-    stock character varying(50),
+    stock character varying(50) DEFAULT 0,
+    amount double precision NOT NULL DEFAULT 0.0,
     available_for_selling boolean NOT NULL DEFAULT false,
     created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE food_store."user"
+CREATE TABLE food_store.user
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     first_name character varying(50) NOT NULL,
@@ -20,7 +21,7 @@ CREATE TABLE food_store."user"
     email character varying(50) NOT NULL,
     address character varying(50) NOT NULL,
     created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    hashed_password character varying(50) NOT NULL,
+    hashed_password character varying(50),
     PRIMARY KEY (id)
 );
 
@@ -29,16 +30,23 @@ CREATE TABLE IF NOT EXISTS food_store.payment
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     user_id integer NOT NULL,
-    products character varying(500) NOT NULL,
+    product_id integer NOT NULL,
     amount double precision NOT NULL DEFAULT 0.0,
     created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT payments_pkey PRIMARY KEY (id)
 );
 
 
+ALTER TABLE food_store.payment ADD CONSTRAINT fk_payments_user FOREIGN KEY (user_id) REFERENCES food_store.user (id);
+ALTER TABLE food_store.payment ADD CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES food_store.product (id);
 
-ALTER TABLE food_store.payments ADD CONSTRAINT fk_payments_user FOREIGN KEY (user_id) REFERENCES food_store.user (id);
 
+
+-- ALTER TABLE food_store ALTER COLUMN product TYPE integer USING (trim(stock)::integer);
+
+
+
+-- ALTER TABLE food_store.product ALTER COLUMN stock DROP NOT NULL;
 
 CREATE INDEX idx_food_store_user_id ON food_store.user (id);
 CREATE INDEX idx_food_store_user_email ON food_store.user (email);
--- script.sql
+++ script.sql
@@ -1,1 +1,57 @@
-s
+-- Function that consults the product and if it meets the restrictions, 
+-- a new record is created in the payment table
+
+CREATE OR REPLACE FUNCTION get_status_product(
+	_prod character varying,
+	_items_to_buy integer,
+	_email character varying
+)
+RETURNS INTEGER 
+AS $$
+	DECLARE 
+	available boolean;
+	total_stock integer;
+	user_id integer;
+	product_id character varying;
+	charge float;
+	new_identity integer;
+	
+	BEGIN
+	-- Wondering if the product is available for sale
+	 available := (SELECT product.available_for_selling
+					FROM food_store.product
+					WHERE product LIKE '%' || _prod || '%');
+	-- check the total in stock
+	total_stock := (SELECT COUNT(product.id) AS total
+					FROM food_store.product
+					WHERE product LIKE '%' || _prod || '%');
+		
+		IF available AND total_stock > 0 AND total_stock <= _items_to_buy THEN
+		-- if it passes all the restrictions
+			user_id := (SELECT id
+						FROM food_store.user
+						WHERE email = _email);
+			product_id := (SELECT product.id
+						FROM food_store.product
+						WHERE product LIKE '%' || _prod || '%');
+						
+			charge  := (SELECT product.amount * _items_to_buy
+						FROM food_store.product
+						WHERE product LIKE '%' || _prod || '%');
+		
+				
+			
+            	INSERT INTO food_store.payment (user_id, product_id, amount, created_at) 
+				VALUES (user_id::INTEGER, product_id::INTEGER, charge::FLOAT, CURRENT_TIMESTAMP) 
+				RETURNING id INTO new_identity;
+				RETURN new_identity;
+		END IF;
+
+	END;
+$$
+LANGUAGE 'plpgsql';
+
+-- Example
+SELECT get_status_product AS id_payment FROM get_status_product('Lid Coffee Cup 8oz Blk', 1, 'aessexm@symantec.com');
+
+
